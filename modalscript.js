44b3a4bab7030018483d933e05bb931391623eb9
Alex Min <alexander.min@gmail.com>
2016-06-17 15:26:22 -0400

    initial modal commit

---
+++ modalscript.js
@@ -0,0 +1,22 @@
+var modal = document.getElementById("mymodal")
+var btn = document.getElementById("btn1")
+var closer = document.getElementById("close")
+
+
+document.getElementById("btn1").onclick = function(event) {
+	modal.setAttribute("style", "display: block")
+}
+
+window.onclick = function(event) {
+	if(event.target != modal && event.target != btn) {
+		modal.setAttribute("style", "display: none")
+	}
+}
+
+closer.onmouseover = function(event) {
+	closer.setAttribute("style", "cursor: hand")
+}
+
+closer.onclick = function(event) {
+	modal.setAttribute("style", "display:none")
+}
